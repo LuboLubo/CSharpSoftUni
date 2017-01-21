8d24bf87e251a92f3b553ccc52a8700f60da1ef0
LuboLubo <lubomir.code@gmail.com>
2017-01-21 20:24:18 +0200

    paste file from previous folder

---
+++ ExamProblems/Programming Basics Exam - 21 February 2016/Programming Basics Exam - 21 February 2016/01 - Hungry Garfield/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem1HungryGarfield
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal JohnMoneyGarfild = decimal.Parse(Console.ReadLine());
+            decimal kursDolar = decimal.Parse(Console.ReadLine());
+            decimal PizzaPrice = decimal.Parse(Console.ReadLine());
+            decimal LasagnalPrice = decimal.Parse(Console.ReadLine());
+            decimal SandvichPrice = decimal.Parse(Console.ReadLine());
+            decimal PizzaQuantityGarfield = decimal.Parse(Console.ReadLine());
+            decimal LasagnalQuantityGarfield = decimal.Parse(Console.ReadLine());
+            decimal SandvichQuantityGarfield = decimal.Parse(Console.ReadLine());
+
+            decimal PizzaDolarsQuantity = (PizzaPrice / kursDolar) * PizzaQuantityGarfield;
+            decimal LasagnalDolarsQuantity = (LasagnalPrice / kursDolar) * LasagnalQuantityGarfield;
+            decimal SandvichDolarsQuantity = (SandvichPrice / kursDolar) * SandvichQuantityGarfield;
+
+            decimal result = PizzaDolarsQuantity + LasagnalDolarsQuantity + SandvichDolarsQuantity;
+            //Console.WriteLine(string.Format("{0:F2}",result ));
+            ////Console.WriteLine(result);
+            if (result < JohnMoneyGarfild)
+            {
+                Console.WriteLine("Garfield is well fed, John is awesome. Money left: ${0:f2}.", JohnMoneyGarfild - result);
+            }
+            else
+            {
+                Console.WriteLine("Garfield is hungry. John is a badass. Money needed: ${0:F2}.", Math.Abs(JohnMoneyGarfild - result));
+            }
+        }
+    }
+}
---
+++ ExamProblems/Programming Basics Exam - 21 February 2016/Programming Basics Exam - 21 February 2016/02 - BeerStock/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            Dictionary<int, Dictionary<string, List<string>>> dicOutput = new Dictionary<int, Dictionary<string, List<string>>>();
+
+            string inputLine = Console.ReadLine();
+            while (!inputLine.Equals("Time for Code"))
+            {
+                string[] inputSplit = inputLine.Split(' ');
+                int id = int.Parse(inputSplit[0]);
+                string name = inputSplit[1];
+                string firstIndex = inputSplit[1];
+                List<string> list = new List<string>(inputSplit.Skip(2));
+                if (firstIndex[0] != '#')
+                {
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+                if (!dicOutput.ContainsKey(id))
+                {
+                    dicOutput.Add(id, new Dictionary<string, List<string>>());
+                    dicOutput[id][name] = list;
+                }
+
+
+                else
+                {
+                    if (!dicOutput[id].ContainsKey(name))
+                    {
+                        inputLine = Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        foreach (var word in list)
+                        {
+                            dicOutput[id][name].Add(word);
+                        }
+                    }
+
+                }
+                inputLine = Console.ReadLine();
+            }
+            Console.WriteLine();
+            foreach (var item in dicOutput.OrderByDescending(x => x.Value.Values.Max(y => y.Count)))
+            {
+                List<string> temporarity = new List<string>();
+                foreach (var ite in item.Value)
+                {
+                    temporarity = ite.Value.Distinct().ToList();
+                    temporarity.Sort();
+                    int count = temporarity.Count;
+                    Console.WriteLine($"{ite.Key.TrimStart('#')} - {count}");
+
+                }
+
+                foreach (var word in temporarity)
+                {
+                    Console.WriteLine($"{word}");
+                }
+
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
